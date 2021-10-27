63039d1aebbbc3f94d4ae351f490ca1b16c4710d
house227 <house1000227@gmail.com>
2021-10-27 16:08:06 +0900

    DBで動こうとしてくれたけど...

--- resources/views/hotel/create_confirm.blade.php
+++ resources/views/hotel/create_confirm.blade.php
@@ -23,7 +23,8 @@
             </tr>
         </table>
 
-        <form action="/hotel/create_db">
+        <form action="/hotel/create_db" method="POST">
+            @csrf
             <input type="hidden" name="confirm_name" value="{{$name}}">
             <input type="hidden" name="confirm_mail" value="{{$mail}}">
             <input type="hidden" name="confirm_tel" value="{{$tel}}">
