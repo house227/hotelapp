46c87c54b234d159c7875ee8e3ae78a0a166713a
house227 <house1000227@gmail.com>
2021-10-28 11:25:38 +0900

    とりあえず登録出来た

--- app/Http/Controllers/HotelController.php
+++ app/Http/Controllers/HotelController.php
@@ -8,11 +8,15 @@ use Illuminate\Support\Facades\DB;
 use App\Http_Requests_HotelEditRequest;
 
 class HotelController extends Controller
+
+
 {
     // 新規ユーザー登録画面を返す
     public function new_user_create(){
         return view('hotel.user_create', ['msg' => '登録に必要なお客様情報を入力してください']);
     }
+
+
 
     // 登録データ確認画面を返す
     public function post_create_data(HotelEditRequest $request){
@@ -26,20 +30,25 @@ class HotelController extends Controller
         return view('hotel.create_confirm', $user_data);
     }
 
+
+
     //ユーザー登録をデータベースへ行う
     public function create_db(Request $request){
 
         $param = [
-            'name' => $request->name,
-            'mail' => $request->mail,
-            'tel' => $request->tel,
+            'name' => $request->confirm_name,
+            'mail' => $request->confirm_mail,
+            'tel' => $request->confirm_tel,
         ];
         DB::table('hotelusers')->insert($param);
-        //とりあえず登録画面へ返す。
+        //とりあえずリダイレクト
         return redirect('/hotel/create_db');
     }
 
-    public function finish_db_create(){
-        return view('hotel.finish_create');
+
+    //登録処理からリダイレクトされて初期の登録画面へ
+    //とりあえず飛ばす。 見分けが付くようにmsgを変更
+    public function next_create(){
+        return view('hotel.user_create', ['msg' => '登録完了しました']);
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -10,5 +10,5 @@ Route::get('hotel', 'HotelController@new_user_create');
 //登録情報確認画面
 Route::post('hotel', 'HotelController@post_create_data');
 
-Route::get('hotel/create_db', 'HotelController@finish_db_create');
+Route::get('hotel/create_db', 'HotelController@next_create');
 Route::post('hotel/create_db', 'HotelController@create_db');
